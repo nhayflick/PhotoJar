df2ce7e3d4c1da1b4ca4cf1b8e2fe81b525e4a07
Nate Hayflick <nhayflick@gmail.com>
2013-04-26 11:36:28 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoJar::Application.config.secret_token = 'fbbcc97b08a33065aa197c120aea213724e425d2b8577911d8b809c512df65ba8328ba5f429105e74f21d45bdb6355f831d8999deab09e4797f6b95a7bc9b486'
